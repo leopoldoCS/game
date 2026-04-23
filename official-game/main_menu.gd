a3890b7d62b3d21b1ca755a7839708558618a231
Sipan Arakelian <arakeliansipan@gmail.com>
2026-04-23 00:24:10 -0700

    resolved merge conflicts after last commit

--- official-game/main_menu.gd
+++ official-game/main_menu.gd
@@ -1,13 +1,10 @@
 extends Control
 
-const MAP_SCENE := preload("res://scenes/map.tscn")
+const MAP_SCENE_PATH := "res://scenes/map.tscn"
+const TUTORIAL_SCENE_PATH := "res://scenes/tutorial_map.tscn"
 
 func _on_start_game_pressed():
-	get_tree().change_scene_to_packed(MAP_SCENE)
+	get_tree().change_scene_to_file(MAP_SCENE_PATH)
 
 func _on_tutorial_pressed():
-	var map = MAP_SCENE.instantiate()
-	map.tutorial_mode = true
-	get_tree().root.add_child(map)
-	get_tree().current_scene = map
-	queue_free()
+	get_tree().change_scene_to_file(TUTORIAL_SCENE_PATH)
