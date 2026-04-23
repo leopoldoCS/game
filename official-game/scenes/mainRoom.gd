4f321c7c8b9957ea397d2609a11c3787ec5468e5
Sipan Arakelian <arakeliansipan@gmail.com>
2026-04-23 00:33:44 -0700

    fixed the "Start Race" button

--- official-game/scenes/mainRoom.gd
+++ official-game/scenes/mainRoom.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/tutorial_map.tscn")
+	get_tree().change_scene_to_file("res://scenes/map.tscn")
 
 func _on_tutorial_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial_map.tscn")
